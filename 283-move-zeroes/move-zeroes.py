a2f4e0b4be5f0a9a2e6354fd0171795d4ca14220
Harshini AP <harshinihh8164419@gmail.com>
2026-07-19 18:56:12 +0530

    Time: 9 ms (32.24%) | Memory: 13.6 MB (48.88%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
